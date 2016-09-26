eaa524f4620c949c1539627e7685270b22a65325
Yuvaraj <yuvaraj119@gmail.com>
2016-09-26 11:33:46 +0530

    First commit

---
+++ app/src/main/java/com/yuvaraj/floatseekbar/FloatSeekBar.java
@@ -0,0 +1,90 @@
+package com.yuvaraj.floatseekbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.SeekBar;
+
+/**
+ * Created by CIPL0349 on 9/23/2016.
+ */
+
+public class FloatSeekBar extends SeekBar {
+
+    private float max = 1.0f;
+    private float min = 0.50f;
+    private float seekValueFloat = 0f;
+
+    public FloatSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public FloatSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatSeekBar(Context context) {
+        super(context);
+    }
+
+    public void setSeekValueFloat(float value) {
+        this.seekValueFloat = value;
+    }
+
+    public float getSeekValueFloat() {
+        return this.seekValueFloat;
+    }
+
+    public void setPlus(float value) {
+        setValue(value + min);
+        seekValueFloat = value + min;
+    }
+
+    public void setMinus(float value) {
+        setValue(value - min);
+        seekValueFloat = value - min;
+    }
+
+    public float getMinFloat() {
+        return this.min;
+    }
+
+    public float getMaxFloat() {
+        return this.max;
+    }
+
+    public void setMaxFloat(float value) {
+        this.max = value;
+        setMax(Math.round(value));
+    }
+
+    public void setMinFloat(float value) {
+        this.min = value;
+    }
+
+    public float getValue() {
+        return Math.round((max - min) * ((float) getProgress() / (float) getMax()) + min);
+    }
+
+    public void setValue(float value) {
+        setProgress(Math.round(value));
+    }
+
+    private void applyAttrs(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FloatSeekBar);
+        final int N = a.getIndexCount();
+        for (int i = 0; i < N; ++i) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.FloatSeekBar_floatMax:
+                    //this.max = a.getFloat(attr, 1.0f);
+                    break;
+                case R.styleable.FloatSeekBar_floatMin:
+                    this.min = a.getFloat(attr, 0.50f);
+                    break;
+            }
+        }
+        a.recycle();
+    }
+}
---
+++ app/src/main/java/com/yuvaraj/floatseekbar/MainActivity.java
@@ -0,0 +1,146 @@
+package com.yuvaraj.floatseekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatSeekBar seekBar, seekBar1;
+    ImageButton button, button2, add, minus;
+    TextView textView,textView1;
+    int clicked = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView1 = (TextView) findViewById(R.id.textView1);
+
+        seekBar = (FloatSeekBar) findViewById(R.id.seekBar);
+        seekBar1 = (FloatSeekBar) findViewById(R.id.seekBar1);
+
+        button = (ImageButton) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        button2 = (ImageButton) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        add = (ImageButton) findViewById(R.id.add);
+        add.setOnClickListener(this);
+        minus = (ImageButton) findViewById(R.id.minus);
+        minus.setOnClickListener(this);
+
+        seekBar.setMaxFloat(10000.0f);
+        seekBar.setMinFloat(0.50f);
+        seekBar.setValue(5.0f);
+        seekBar.setSeekValueFloat(5.0f);
+
+        seekBar1.setMaxFloat(10000.0f);
+        seekBar1.setMinFloat(0.50f);
+        seekBar1.setValue(5.0f);
+        seekBar1.setSeekValueFloat(5.0f);
+
+        textView.setText("" + seekBar.getSeekValueFloat());
+        textView1.setText("" + seekBar1.getSeekValueFloat());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBars, int progress, boolean fromUser) {
+                if (clicked == 1) {//add
+                    clicked = 0;
+                } else if (clicked == 2) {//minus
+                    clicked = 0;
+                } else {
+                    seekBar.setSeekValueFloat(seekBar.getValue());
+                    clicked = 0;
+                }
+
+                textView.setText("" + seekBar.getSeekValueFloat());
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBars, int progress, boolean fromUser) {
+                if (clicked == 1) {//add
+                    clicked = 0;
+                } else if (clicked == 2) {//minus
+                    clicked = 0;
+                } else {
+                    seekBar1.setSeekValueFloat(seekBar1.getValue());
+                    clicked = 0;
+                }
+
+                textView1.setText("" + seekBar1.getSeekValueFloat());
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.button2:
+                clicked = 1;
+                if (seekBar.getSeekValueFloat() < seekBar.getMaxFloat()) {
+                    seekBar.setPlus(seekBar.getSeekValueFloat());
+                    Log.e("seekbar value plus", "" + seekBar.getSeekValueFloat());
+                    textView.setText("" + seekBar.getSeekValueFloat());
+                }
+                break;
+            case R.id.button:
+                clicked = 2;
+                if (seekBar.getSeekValueFloat() > seekBar.getMinFloat()) {
+                    seekBar.setMinus(seekBar.getSeekValueFloat());
+                    Log.e("seekbar value minus", "" + seekBar.getSeekValueFloat());
+                    textView.setText("" + seekBar.getSeekValueFloat());
+                }
+                break;
+            case R.id.add:
+                clicked = 1;
+                if (seekBar1.getSeekValueFloat() < seekBar1.getMaxFloat()) {
+                    seekBar1.setPlus(seekBar1.getSeekValueFloat());
+                    Log.e("seekbar1 value plus", "" + seekBar1.getSeekValueFloat());
+                    textView1.setText("" + seekBar1.getSeekValueFloat());
+                }
+                break;
+            case R.id.minus:
+                clicked = 2;
+                if (seekBar1.getSeekValueFloat() > seekBar1.getMinFloat()) {
+                    seekBar1.setMinus(seekBar1.getSeekValueFloat());
+                    Log.e("seekbar1 value minus", "" + seekBar1.getSeekValueFloat());
+                    textView1.setText("" + seekBar1.getSeekValueFloat());
+                }
+                break;
+        }
+    }
+}
